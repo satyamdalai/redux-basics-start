8827d4ccbb47bf280a6e2913ea6b8d0fce140076
Satyam Dalai <54313146+satyamdalai@users.noreply.github.com>
2023-06-22 19:09:26 +0000

    Update Counter.js and index.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { useSelector, useDispatch, connect } from "react-redux";
 
 import classes from "./Counter.module.css";
+import { counterActions } from "../store/index";
 
 const Counter = () => {
   const dispatch = useDispatch();
@@ -9,17 +10,17 @@ const Counter = () => {
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5));
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
